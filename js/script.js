53854c950f38521e94fecc2150c017b16ab7e631
Andrey Kozlov <andrew08552gmail.com>
2023-03-28 17:22:56 +0300

    second point ready

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 "use strict";
 
+// ___________________1Item____________
+// let user = {
+//     name:"Andrey",
+//     am:undefined,
+//     age:19,
+// };
 
-let user = {
-    name:"Andrey",
-    age:19,
-}
-const isPlainObject = value => value.constructor === Object;
-console.log(isPlainObject(user))
+// const isPlainObject = value => value.constructor === Object;
+// console.log(isPlainObject(user))
+
+// ___________________2Item____________
+
+// let check_for_emptiness = (value) =>{
+//     if (Object.keys(value).length !== 0){
+//         for (let i in value){
+//             if (value[i] !== undefined || null) return "Объект не пуст";
+//         }
+//     }
+//     return "Объект пуст!"
+// }
+// console.log(check_for_emptiness(user))
